64d5f1d44b15cca1ad9de6542a11ca6a0d64d55b
lihaijun7781 <lihaijun7781@hotmail.com>
2020-08-02 22:50:39 +0800

    fix compile error

--- utility/board/everylinked/Inc/board_drv_led.h
+++
@@ -1,8 +0,0 @@
-/*
-* @ function comments
-* everylinked privide one LED for users to control,
-* here is the interface to turn on/off this LED inside the board
-* @ input para: bool(1/0) which means gpio active level
-* @ return para: void
-*/
-void board_drv_led_ctrl(bool active);
